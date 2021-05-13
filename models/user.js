e15c82879b7d0f8f3f75ae98ac918e46bd145de5
Noncenze <thmas.dee@gmail.com>
2021-05-13 12:59:15 -0400

    User: add functions hashing and validating functions to password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt');
 const {
   Model
 } = require('sequelize');
@@ -44,6 +45,22 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
+
+  user.addHook('beforeCreate', (pendingUser) => {
+    let hash = bcrypt.hashSync(pendingUser.password, 12);
+    pendingUser.password = hash;
+  });
+
+  user.prototype.validPassword = function(typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password);
+    return isCorrectPassword;
+  };
+
+  user.prototype.toJSON = function() {
+    let userData = this.get();
+    delete userData.password;
+    return userData;
+  };
 
   return user; // add functions above 
 };
