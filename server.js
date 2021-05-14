a042f945569815055a73c2ef56499e1a88a01e4c
Noncenze <thmas.dee@gmail.com>
2021-05-14 11:46:35 -0400

    Server.js: Move passport use below flash

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(session({
 }))
 
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   console.log(res.locals);
@@ -31,23 +33,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.get('/', (req, res) => {
   res.render('index');
 });
-
-// app.get('/profile', (req, res) => {
-//   res.render('profile');
-// });
 
 app.use('/auth', require('./controllers/auth'));
 
 app.get('/profile', isLoggedIn, (req, res) => {
   const {id, name, email} = req.user.get();
   res.render('profile', {id, name, email});
-});
+})
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
