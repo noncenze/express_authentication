55f59b326f7ea2b5a0e258d748ed89aebf3b49f7
Noncenze <thmas.dee@gmail.com>
2021-05-14 11:19:58 -0400

    Profile: add route and send data to view page

--- server.js
+++ server.js
@@ -38,12 +38,16 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/profile', (req, res) => {
-  res.render('profile');
-});
+// app.get('/profile', (req, res) => {
+//   res.render('profile');
+// });
 
 app.use('/auth', require('./controllers/auth'));
 
+app.get('/profile', isLoggedIn, (req, res) => {
+  const {id, name, email} = req.user.get();
+  res.render('profile', {id, name, email});
+});
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
